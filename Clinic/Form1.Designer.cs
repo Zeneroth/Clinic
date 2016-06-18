c17497fd3c04dbaa2aed144d36e0c595ab11bcb0
Zeneroth <pattagok@gmail.com>
2016-06-18 16:41:51 +0700

    basic functionalities

--- Clinic/Form1.Designer.cs
+++ Clinic/Form1.Designer.cs
@@ -30,24 +30,26 @@
         {
             this.nameTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.resultRichTextBox = new System.Windows.Forms.RichTextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.CSVFiletextBox = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.resultRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.registerNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.registerName = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.backupButton = new System.Windows.Forms.Button();
+            this.CSVFiletextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.csvBrowseButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.registerSuccessLabel = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -72,15 +74,15 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Search Keyword";
             // 
-            // button1
+            // searchButton
             // 
-            this.button1.Location = new System.Drawing.Point(560, 35);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Search";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.searchButton.Location = new System.Drawing.Point(560, 35);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 4;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButtonClick);
             // 
             // tabControl1
             // 
@@ -98,7 +100,7 @@
             this.tabPage1.Controls.Add(this.label3);
             this.tabPage1.Controls.Add(this.resultRichTextBox);
             this.tabPage1.Controls.Add(this.label1);
-            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.searchButton);
             this.tabPage1.Controls.Add(this.nameTextBox);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
@@ -107,49 +109,6 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Search";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Controls.Add(this.label6);
-            this.tabPage2.Controls.Add(this.textBox2);
-            this.tabPage2.Controls.Add(this.label2);
-            this.tabPage2.Controls.Add(this.textBox1);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(670, 468);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Register";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // tabPage3
-            // 
-            this.tabPage3.Controls.Add(this.textBox4);
-            this.tabPage3.Controls.Add(this.label5);
-            this.tabPage3.Controls.Add(this.button3);
-            this.tabPage3.Controls.Add(this.CSVFiletextBox);
-            this.tabPage3.Controls.Add(this.label4);
-            this.tabPage3.Controls.Add(this.button2);
-            this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(670, 468);
-            this.tabPage3.TabIndex = 2;
-            this.tabPage3.Text = "Setting";
-            this.tabPage3.UseVisualStyleBackColor = true;
-            // 
-            // resultRichTextBox
-            // 
-            this.resultRichTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.resultRichTextBox.Location = new System.Drawing.Point(37, 125);
-            this.resultRichTextBox.Name = "resultRichTextBox";
-            this.resultRichTextBox.Size = new System.Drawing.Size(598, 317);
-            this.resultRichTextBox.TabIndex = 5;
-            this.resultRichTextBox.Text = "";
-            this.resultRichTextBox.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // label3
             // 
@@ -161,72 +120,30 @@
             this.label3.TabIndex = 6;
             this.label3.Text = "Results";
             // 
-            // button2
+            // resultRichTextBox
             // 
-            this.button2.Location = new System.Drawing.Point(461, 23);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "Browse";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            this.resultRichTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultRichTextBox.Location = new System.Drawing.Point(37, 125);
+            this.resultRichTextBox.Name = "resultRichTextBox";
+            this.resultRichTextBox.Size = new System.Drawing.Size(598, 317);
+            this.resultRichTextBox.TabIndex = 5;
+            this.resultRichTextBox.Text = "";
             // 
-            // label4
+            // tabPage2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(28, 28);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(56, 13);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "CSV File : ";
-            // 
-            // CSVFiletextBox
-            // 
-            this.CSVFiletextBox.Location = new System.Drawing.Point(99, 25);
-            this.CSVFiletextBox.Name = "CSVFiletextBox";
-            this.CSVFiletextBox.Size = new System.Drawing.Size(327, 20);
-            this.CSVFiletextBox.TabIndex = 2;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(115, 64);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(311, 20);
-            this.textBox4.TabIndex = 5;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(28, 67);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(81, 13);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Backup Email : ";
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(461, 62);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Backup";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(70, 19);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(363, 20);
-            this.textBox1.TabIndex = 0;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(20, 22);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Name";
+            this.tabPage2.Controls.Add(this.registerSuccessLabel);
+            this.tabPage2.Controls.Add(this.registerButton);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.registerNumber);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.registerName);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(670, 468);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Register";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // label6
             // 
@@ -237,12 +154,118 @@
             this.label6.TabIndex = 3;
             this.label6.Text = "Number";
             // 
-            // textBox2
+            // registerNumber
             // 
-            this.textBox2.Location = new System.Drawing.Point(506, 19);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(135, 20);
-            this.textBox2.TabIndex = 2;
+            this.registerNumber.Location = new System.Drawing.Point(506, 19);
+            this.registerNumber.Name = "registerNumber";
+            this.registerNumber.Size = new System.Drawing.Size(135, 20);
+            this.registerNumber.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // registerName
+            // 
+            this.registerName.Location = new System.Drawing.Point(70, 19);
+            this.registerName.Name = "registerName";
+            this.registerName.Size = new System.Drawing.Size(363, 20);
+            this.registerName.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.emailTextBox);
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Controls.Add(this.backupButton);
+            this.tabPage3.Controls.Add(this.CSVFiletextBox);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.csvBrowseButton);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(670, 468);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Setting";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(115, 64);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(311, 20);
+            this.emailTextBox.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Backup Email : ";
+            // 
+            // backupButton
+            // 
+            this.backupButton.Location = new System.Drawing.Point(461, 62);
+            this.backupButton.Name = "backupButton";
+            this.backupButton.Size = new System.Drawing.Size(75, 23);
+            this.backupButton.TabIndex = 3;
+            this.backupButton.Text = "Backup";
+            this.backupButton.UseVisualStyleBackColor = true;
+            // 
+            // CSVFiletextBox
+            // 
+            this.CSVFiletextBox.Location = new System.Drawing.Point(99, 25);
+            this.CSVFiletextBox.Name = "CSVFiletextBox";
+            this.CSVFiletextBox.Size = new System.Drawing.Size(327, 20);
+            this.CSVFiletextBox.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "CSV File : ";
+            // 
+            // csvBrowseButton
+            // 
+            this.csvBrowseButton.Location = new System.Drawing.Point(461, 23);
+            this.csvBrowseButton.Name = "csvBrowseButton";
+            this.csvBrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.csvBrowseButton.TabIndex = 0;
+            this.csvBrowseButton.Text = "Browse";
+            this.csvBrowseButton.UseVisualStyleBackColor = true;
+            this.csvBrowseButton.Click += new System.EventHandler(this.csvBrowseButtonClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // registerButton
+            // 
+            this.registerButton.Location = new System.Drawing.Point(566, 57);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(75, 23);
+            this.registerButton.TabIndex = 4;
+            this.registerButton.Text = "Register";
+            this.registerButton.UseVisualStyleBackColor = true;
+            this.registerButton.Click += new System.EventHandler(this.registerButtonClick);
+            // 
+            // registerSuccessLabel
+            // 
+            this.registerSuccessLabel.AutoSize = true;
+            this.registerSuccessLabel.Location = new System.Drawing.Point(551, 98);
+            this.registerSuccessLabel.Name = "registerSuccessLabel";
+            this.registerSuccessLabel.Size = new System.Drawing.Size(90, 13);
+            this.registerSuccessLabel.TabIndex = 5;
+            this.registerSuccessLabel.Text = "Register Success";
+            this.registerSuccessLabel.Visible = false;
             // 
             // Form1
             // 
@@ -251,7 +274,7 @@
             this.ClientSize = new System.Drawing.Size(702, 518);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Clinic";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
@@ -267,7 +290,7 @@
 
         private System.Windows.Forms.TextBox nameTextBox;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button searchButton;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
@@ -275,16 +298,18 @@
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.RichTextBox resultRichTextBox;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox emailTextBox;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button backupButton;
         private System.Windows.Forms.TextBox CSVFiletextBox;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button csvBrowseButton;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox registerNumber;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox registerName;
+        private System.Windows.Forms.Label registerSuccessLabel;
+        private System.Windows.Forms.Button registerButton;
     }
 }
 
--- Clinic/Form1.cs
+++ Clinic/Form1.cs
@@ -16,22 +16,14 @@ namespace Clinic
     {
         public string CSVPath = "C:\\Users\\Gok\\Desktop\\clinic.csv";
         public string backupEmail;
+        public string configPath = "config.txt";
         public Form1()
         {
             InitializeComponent();
+            readConfigFile();
         }
 
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
-
-        }
-
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click_1(object sender, EventArgs e)
+        private void csvBrowseButtonClick(object sender, EventArgs e)
         {
             // Show the dialog and get result.
             DialogResult result = openFileDialog1.ShowDialog();
@@ -40,15 +32,25 @@ namespace Clinic
                 Console.WriteLine(openFileDialog1.FileName);
                 CSVFiletextBox.Text = openFileDialog1.FileName;
                 CSVPath = openFileDialog1.FileName;
+                using (StreamWriter sw = File.CreateText(configPath))
+                {
+                    sw.WriteLine("csvpath=" + CSVPath + "\n");
+                    sw.WriteLine("email=" + backupEmail + "\n");
+                }
             }
             
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void searchButtonClick(object sender, EventArgs e)
         {
+            if (CSVPath.Length == 0)
+            {
+                MessageBox.Show("CSV path is not set!");
+                return;
+            }
             try
             {
-
+                resultRichTextBox.Text = "";
                 string[] lines = File.ReadAllLines(CSVPath);
                 for (int i = 0; i < lines.Length; i++)
                 {
@@ -65,6 +67,36 @@ namespace Clinic
                 MessageBox.Show("Error is " + ex.ToString());
                 throw;
             }
+        }
+
+        private void registerButtonClick(object sender, EventArgs e)
+        {
+            if (registerName.Text.Length == 0 || registerNumber.Text.Length == 0) {
+                MessageBox.Show("Please input both Name and Number!");
+                return;
+            }
+
+            register(registerName.Text, registerNumber.Text);
+        }
+
+        public void register(string name, string number) {
+         
+            using (StreamWriter sw = File.AppendText(CSVPath))
+            {
+                sw.WriteLine(name + ',' + number + "," + DateTime.Now.GetDateTimeFormats()[103]);
+            }
+            registerSuccessLabel.Visible = true;
+        }
+
+        private void readConfigFile() {
+            string[] lines = File.ReadAllLines("config.txt");
+            if(lines[0].Split('=').Length > 1)
+                CSVPath = lines[0].Split('=')[1];
+            if (lines[1].Split('=').Length > 1 )
+                backupEmail = lines[1].Split('=')[1];
+            CSVFiletextBox.Text = CSVPath;
+            emailTextBox.Text = backupEmail;
+            Console.WriteLine("csv=" + CSVPath + " email=" + backupEmail);
         }
     }
 }
